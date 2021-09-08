caff615c038a461fa639d1a71b8f0a29e131a074
emamul078 <emamul@kolpolok.com>
2021-09-08 16:02:37 +0600

    fibonnachi-updated

--- MyFancyJavaProjectApplication.java
+++ MyFancyJavaProjectApplication.java
@@ -2,9 +2,10 @@ public class MyFancyJavaProjectApplication {
  public static void main(String args[])  
   {    
     int n1=0,n2=1,n3,i,count=10;    
-    System.out.print(n1+" "+n2);//printing 0 and 1    
-    
-    for(i=2;i<count;++i)//loop starts from 2 because 0 and 1 are already printed    
+
+System.out.println("Fibonacci Series till " + n + " terms:");  
+  
+    for(i=2;i<count;++i)   
    {    
      n3=n1+n2;    
      System.out.print(" "+n3);    
